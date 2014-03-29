577e903731af30897b731881b90e14228c9f203f
MartinBodocky <bodocky@gmail.com>
2014-03-29 13:26:22 +0000

    new version

--- SignalRSelfHostFSharp/Program.fs
+++ SignalRSelfHostFSharp/Program.fs
@@ -1,18 +1,25 @@
 ﻿namespace Program
 
 open Owin
-open Dynamic
 open Microsoft.AspNet.SignalR
 open Microsoft.AspNet.SignalR.Hubs
 open Microsoft.Owin.Hosting
 open Microsoft.Owin.Cors
 open System
 open System.Diagnostics
+open EkonBenefits.FSharp.Dynamic
+
 
 type MyHub =
     inherit Hub
     member x.Send (name : string) (message : string) =
+        base.Clients.All?addMessage(name,message) |> ignore
+(*
+type MyHub =
+    inherit Hub
+    member x.Send (name : string) (message : string) =
         base.Clients.All?addMessage name message
+
 
 type MyWebStartUp() =
     member x.Configuration (app :IAppBuilder) =
@@ -20,13 +27,20 @@ type MyWebStartUp() =
         app.MapSignalR() |> ignore
         ()
 
+*)
+
 module Starter =
 
     [<EntryPoint>]
     let main argv = 
+        let startup (a:IAppBuilder) =
+            a.UseCors(CorsOptions.AllowAll) |> ignore
+            a.MapSignalR() |> ignore
+
         let hostUrl = "http://localhost:8085"
-        let disposable = WebApp.Start<MyWebStartUp>(hostUrl)
+        use app = WebApp.Start(hostUrl, startup)
+        //let disposable = WebApp.Start<MyWebStartUp>(hostUrl)
         Console.WriteLine("Server running on "+ hostUrl)
         Console.ReadLine() |> ignore
-        disposable.Dispose() 
+        //disposable.Dispose() 
         0 // return an integer exit code
